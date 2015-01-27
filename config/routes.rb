827d28a89abca48fa966c807a489f5be42b8223e
41407 <jimi.welling@cs.helsinki.fi>
2015-01-27 15:59:09 +0200

    Add model Hypothesis

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :hypotheses
+
   resources :tasks
 
   resources :exercises
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127135131) do
+ActiveRecord::Schema.define(version: 20150127135659) do
 
   create_table "exercises", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hypotheses", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
