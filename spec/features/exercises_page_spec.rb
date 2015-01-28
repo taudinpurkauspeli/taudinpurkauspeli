3c90b56689666f762d2571802530aacb604b238b
Eveliina Pakarinen <eve.pakarinen@gmail.com>
2015-01-28 11:54:24 +0200

    Fixed tests

--- spec/features/exercises_page_spec.rb
+++ spec/features/exercises_page_spec.rb
@@ -4,7 +4,7 @@ describe "Exercises page" do
 
   it "should have right title" do
     visit exercises_path
-    expect(page).to have_content 'Exercises'
+    expect(page).to have_content 'Taudinpurkauspeli'
   end
 
 
