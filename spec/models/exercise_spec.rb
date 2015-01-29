6f552c786b76e280d323230836085bcea786abec
Dani Nu <kiurun@1BQQFVW.kumpula.dhcp.hupnet.helsinki.fi>
2015-01-29 14:27:36 +0200

    exercise validation

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -8,6 +8,24 @@ describe Exercise do
 
   end
 
+
+   it "is not saved without a name" do
+    exercise = Exercise.create name:"", anamnesis:"Koira sairastaa :("
+
+    expect(exercise).not_to be_valid
+    expect(Exercise.count).to eq(0)
+
+  end
+
+     it "is not saved with a too short name" do
+    exercise = Exercise.create name:"h", anamnesis:"Koira sairastaa :("
+
+    expect(exercise).not_to be_valid
+    expect(Exercise.count).to eq(0)
+
+  end
+
+
    it "has the anamnesis set correctly" do
     exercise = Exercise.new name:"Ripuloiva koira", anamnesis:"Koira sairastaa :("
 
@@ -23,6 +41,13 @@ describe Exercise do
     it "is saved" do
       expect(exercise).to be_valid
       expect(Exercise.count).to eq(1)
+    end
+
+    it "is not saved if the name is already in use" do
+      exercise2 = Exercise.create name:"Lihanautakuolemat", anamnesis:"Voi voi koiraa"
+
+      expect(exercise2).not_to be_valid
+      expect(Exercise.count).to eq(1) 
     end
   end
 
