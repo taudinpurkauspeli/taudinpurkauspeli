1d94891a370999403f9a0008a732dc9667566bbc
41407 <jimi.welling@cs.helsinki.fi>
2015-01-30 14:24:24 +0200

    Add cucumber tests

---
+++ features/step_definitions/task_index_steps.rb
@@ -0,0 +1,25 @@
+begin
+  require 'rspec/expectations';
+rescue LoadError;
+  require 'spec/expectations';
+end
+require 'cucumber/formatter/unicode'
+$:.unshift(File.dirname(__FILE__) + '/../../lib')
+
+Given(/^I am on the front page with preexisting exercises which have tasks$/) do
+  Exercise.create name:"Lihanautakuolemat", anamnesis:"Lihanautoja on kuollut"
+  Task.create name:"Soita lääkärille"
+  
+  visit exercises_path
+end
+
+And(/^I click on the link "(.*?)"$/) do |variable|
+  click_link(variable)
+end
+
+When(/^I click on the link "(.*?)"$/) do |variable|
+  click_link(variable)
+end
+Then(/^the page should have a link: "(.*?)"$/) do |variable|
+  expect(page).to have_content variable
+end
---
+++ features/task_index.feature
@@ -0,0 +1,9 @@
+Feature: New exercise feature
+  As a user
+  I want to view the task list of an exercise
+
+  Scenario: User can view the task list of an exercise
+    Given I am on the front page with preexisting exercises which have tasks
+    And I click on the link "Lihanautakuolemat"
+    When I click on the link "Toimenpiteet"
+    Then the page should have a link: "Soita lääkärille"
