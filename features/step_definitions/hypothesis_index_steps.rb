f2bb4cd4b8724739964cabc8c30549115a9ea4a6
41407 <jimi.welling@cs.helsinki.fi>
2015-01-30 14:40:18 +0200

    Add cucumber tests for hypothesis

--- features/step_definitions/hypothesis_index_steps.rb
+++ features/step_definitions/hypothesis_index_steps.rb
@@ -6,9 +6,9 @@ end
 require 'cucumber/formatter/unicode'
 $:.unshift(File.dirname(__FILE__) + '/../../lib')
 
-Given(/^I am on the front page with preexisting exercises which have tasks$/) do
+Given(/^I am on the front page with preexisting exercises which have hypotheses$/) do
   Exercise.create name:"Lihanautakuolemat", anamnesis:"Lihanautoja on kuollut"
-  Task.create name:"Soita lääkärille"
+  Hypothesis.create name:"Nautaflunssa"
 
   visit exercises_path
 end
@@ -21,6 +21,6 @@ When(/^I click on the link "(.?)"$/) do |variable|
   click_link(variable)
 end
 
-Then(/^the page should have a link: "(.?)"$/) do |arg1|
+Then(/^the page should have a link: "(.*?)"$/) do |arg1|
   expect(page).to have_content(arg1)
 end
