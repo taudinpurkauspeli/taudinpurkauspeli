f284ccdda30e3d61917ff380c041ded0b19dadfc
Eveliina Pakarinen <eve.pakarinen@gmail.com>
2015-02-03 15:50:29 +0200

    Restored old test

--- spec/views/exercises/index.html.erb_spec.rb
+++ spec/views/exercises/index.html.erb_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "exercises/index", :type => :view do
 
   it "renders a list of exercises" do
     render
-    assert_select "btn-exercise", :text => "Name".to_s, :count => 1
-    assert_select "btn-exercise", :text => "Name2".to_s, :count => 1
+    assert_select "tr>td", :text => "Name".to_s, :count => 1
+    assert_select "tr>td", :text => "Name2".to_s, :count => 1
   end
 end
