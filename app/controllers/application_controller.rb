d308f0e5776be071c7d51d9f1671d7b16038d5ac
41407 <jimi.welling@cs.helsinki.fi>
2015-02-06 15:01:28 +0200

    Add method to check for adminness

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # määritellään, että metodi current_user tulee käyttöön myös näkymissä
+  # määritellään, että metodit tulevat käyttöön myös näkymissä
   helper_method :current_user
+  helper_method :current_user_is_admin
 
   def current_user
     return nil if session[:user_id].nil? 
     User.find(session[:user_id]) 
+  end
+
+  def current_user_is_admin
+  	u = current_user
+  	if !u.nil? 
+  		return u.admin
+  	end
+  	return false
   end
 end
