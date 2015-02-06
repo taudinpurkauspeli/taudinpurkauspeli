8ea0431bde2fd6c9133e837295cbce084d002bd4
41407 <jimi.welling@cs.helsinki.fi>
2015-02-06 13:49:03 +0200

    Add user, session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+    def new
+      # renderöi kirjautumissivun
+    end
+
+    def create
+      # haetaan usernamea vastaava käyttäjä tietokannasta
+      user = User.find_by username: params[:username]
+      # talletetaan sessioon kirjautuneen käyttäjän id (jos käyttäjä on olemassa)
+      session[:user_id] = user.id if not user.nil?
+      # uudelleen ohjataan käyttäjä omalle sivulleen 
+
+      # RUMA JA PASKA
+      if user.nil?
+        redirect_to new_session_path
+      else 
+        redirect_to user        
+      end
+    end
+
+    def destroy
+      # nollataan sessio
+      session[:user_id] = nil
+      # uudelleenohjataan sovellus pääsivulle 
+      redirect_to :root
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127151336) do
+ActiveRecord::Schema.define(version: 20150206110237) do
 
   create_table "exercise_hypotheses", force: :cascade do |t|
     t.integer  "exercise_id"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 20150127151336) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "exercise_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
