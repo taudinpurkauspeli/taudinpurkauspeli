e4f1720202254d43e9fa095ca79bf0f1aafc10e9
41407 <jimi.welling@cs.helsinki.fi>
2015-02-09 15:50:29 +0200

    Fix cucumbers

---
+++ features/step_definitions/generic_steps.rb
@@ -0,0 +1,64 @@
+begin
+  require 'rspec/expectations';
+rescue LoadError;
+  require 'spec/expectations';
+end
+require 'cucumber/formatter/unicode'
+$:.unshift(File.dirname(__FILE__) + '/../../lib')
+
+When(/^I click on the link "(.*?)"$/) do |arg1|
+  click_link(arg1)
+end
+
+When(/^I click on the button "(.*?)"$/) do |arg1|
+  click_button(arg1)
+end
+
+# TODO join ^
+When(/^I press "(.*?)"$/) do |arg1|
+  click_button(arg1)
+end
+
+Then(/^the page should show the content "(.*?)"$/) do |string|
+  expect(page).to have_content string
+end
+
+# TODO join ^
+Then(/^page should have message: "(.*?)"$/) do |arg1|
+  expect(page).to have_content arg1
+end
+
+Then(/the page should have button "(.*?)"$/) do |arg1|
+	expect(page). to have_button arg1
+end
+
+Given(/I have logged in as a student$/) do
+  user = FactoryGirl.create(:user, admin: false)
+  visit signin_path
+  fill_in('username', with:"Testipoika")
+  fill_in('password', with:"Salainen1")
+  click_button('Log in')
+end
+
+Given(/I have logged in as a teacher$/) do
+  user = FactoryGirl.create(:user, admin: true)
+  visit signin_path
+  fill_in('username', with:"Testipoika")
+  fill_in('password', with:"Salainen1")
+  click_button('Log in')
+end
+
+Given(/^exercises have been added$/) do
+  Exercise.create name:"Lihanautakuolemat", anamnesis:"Lihanautoja on menehtynyt lukuisia"
+  Exercise.create name:"Heikko hevonen", anamnesis:"Hevosella on heikot polvet"
+end
+
+Given(/^hypotheses have been added$/)  do
+  Hypothesis.create name:"Nautaflunssa"
+  Hypothesis.create name:"Hevosheikkous"
+end
+
+Given(/^tasks have been added$/)  do
+  Task.create name:"Soita lääkärille"
+  Task.create name:"Lääkitse hevonen"
+end
