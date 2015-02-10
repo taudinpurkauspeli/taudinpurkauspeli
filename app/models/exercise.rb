a99fd7158c39c9ebc6079ce94c2c566697605066
41407 <jimi.welling@cs.helsinki.fi>
2015-02-10 17:10:02 +0200

    Add validations for hypothesis_group

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,7 +1,5 @@
 class Exercise < ActiveRecord::Base
-
 	validates :name, presence: true, uniqueness: true, length: {minimum: 2}
-
 
 	has_many :tasks
 	has_many :exercise_hypotheses
--- app/models/hypothesis_group.rb
+++ app/models/hypothesis_group.rb
@@ -1,3 +1,4 @@
 class HypothesisGroup < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
 	has_many :hypotheses
 end
