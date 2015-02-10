d7a084871484a58118240ca459edbba42280ca14
41407 <jimi.welling@cs.helsinki.fi>
2015-02-10 16:17:05 +0200

    Connect hypothesisGroup -1---*- hypothesis

--- app/models/hypothesis.rb
+++ app/models/hypothesis.rb
@@ -1,4 +1,5 @@
 class Hypothesis < ActiveRecord::Base
 	has_many :exercise_hypotheses
 	has_many :exercises, through: :exercise_hypotheses
+  belongs_to :hypothesis_group
 end
--- app/models/hypothesis_group.rb
+++ app/models/hypothesis_group.rb
@@ -1,3 +1,3 @@
 class HypothesisGroup < ActiveRecord::Base
-
+	has_many :hypotheses
 end
