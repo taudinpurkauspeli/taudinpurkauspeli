1dab4509d89eeac83391d2717be4554e19e3bc79
41407 <jimi.welling@cs.helsinki.fi>
2015-02-11 11:02:29 +0200

    Test current exercise shows correct anamnesis

--- features/step_definitions/anamnesis_steps.rb
+++ features/step_definitions/anamnesis_steps.rb
@@ -5,8 +5,3 @@ rescue LoadError;
 end
 require 'cucumber/formatter/unicode'
 $:.unshift(File.dirname(__FILE__) + '/../../lib')
-
-Given(/^I am on the front page with preexisting exercises which have anamneses$/) do
-  Exercise.create name:"Lihanautakuolemat", anamnesis:"Lihanautoja on menehtynyt lukuisia"
-  visit exercises_path
-end
