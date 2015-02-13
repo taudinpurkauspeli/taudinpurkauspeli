63b541a2e114af65738e1879ea2104aeb34187a8
41407 <jimi.welling@cs.helsinki.fi>
2015-02-13 14:08:10 +0200

    Fix ex_hyp controller redirects, notices

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,6 +2,7 @@ class Exercise < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true, length: {minimum: 2}
 
 	has_many :tasks
+  
 	has_many :exercise_hypotheses
 	has_many :hypotheses, through: :exercise_hypotheses
 end
