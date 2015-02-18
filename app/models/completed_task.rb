87923f9f421d1d686909339630f7008eebc9c9b4
41407 <jimi.welling@cs.helsinki.fi>
2015-02-18 13:45:39 +0200

    Connect freshly created tables we created freshly

--- app/models/completed_task.rb
+++ app/models/completed_task.rb
@@ -1,2 +1,9 @@
 class CompletedTask < ActiveRecord::Base
+  validates :task_id, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :user
+  belongs_to :task
+
+  has_one :exercise, through: :task
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
 	belongs_to :exercise
+  
+  has_many :users, through: :completed_tasks
 end
