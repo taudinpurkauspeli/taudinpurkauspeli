bb2aac5a25f14251e4e85f759f25e3e7b1a6fda9
41407 <jimi.welling@cs.helsinki.fi>
2015-02-27 14:43:49 +0200

    Finish new task stuff

--- app/controllers/task_texts_controller.rb
+++ app/controllers/task_texts_controller.rb
@@ -23,14 +23,18 @@ class TaskTextsController < ApplicationController
   def create
     @task = Task.find(session[:task_id])
 
+    # This can be done for each different type of subtask in their respective controllers
     subtask = @task.subtasks.create
-    task_text = subtask.create_task_text(content:task_text_params[:content])
+    @task_text = subtask.create_task_text(content:task_text_params[:content])
 
     respond_to do |format|
-      if task_text.save
-        format.html { redirect_to task_text.subtask.task, notice: 'TaskText was successfully created.' }
-        format.json { render :show, status: :created, location: task_text }
+      if @task_text.save
+        format.html { redirect_to @task_text.subtask.task, notice: 'TaskText was successfully created.' }
+        format.json { render :show, status: :created, location: @task_text }
       else
+        # This is chewing gum
+        @task_text.delete
+        subtask.delete
         format.html { render :new }
         format.json { render json: @task_text.errors, status: :unprocessable_entity }
       end
