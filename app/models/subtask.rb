0ebedf8abd8a9b94ac8e0c0150c4a8887afcff02
Dani Nu <kiurun@1BQQGOW.kumpula.dhcp.hupnet.helsinki.fi>
2015-03-09 16:59:48 +0200

    subtask to_string

--- app/models/subtask.rb
+++ app/models/subtask.rb
@@ -6,4 +6,22 @@ class Subtask < ActiveRecord::Base
 
 	has_one :task_text, dependent: :destroy
 	has_one :multichoice, dependent: :destroy
+
+	def to_s
+		return_string = 'Alitoimenpide'
+		content = ''
+
+		unless task_text.nil?
+			return_string = 'Teksti: '
+			content = task_text.content
+			
+		end
+		unless multichoice.nil?
+			return_string = 'Monivalintakysymys: '
+			content = multichoice.question
+
+		end
+		return_string += content.split[0...3].join(' ') + ' ...'
+		return return_string
+	end
 end
