598d60fc62c9400eb16f66ccf23339b254849ab7
Eveliina <pevetiainen.ep@hotmail.com>
2015-03-18 10:47:45 +0200

    Fixed teacher preview for multichoice

--- spec/features/show_task_page_spec.rb
+++ spec/features/show_task_page_spec.rb
@@ -15,7 +15,7 @@ describe "Task show page" do
   let!(:option3){FactoryGirl.create(:option, content: "Ehkä tykkää", explanation: "Melkein oikea vastaus")}
 
 
-  describe "if user is signed in as student" do
+  describe "if user is signed in as a student" do
 
     let!(:user){FactoryGirl.create(:student)}
 
@@ -34,13 +34,53 @@ describe "Task show page" do
       expect(page).to have_content 'Lääkäri kertoo mikä on totuus'
     end
 
-    it "user should be able to view the question and answer options for multichoice task" do
+    it "user should be able to view the question and answer options for a multichoice task" do
       click_button(multichoice_task.name)
 
       expect(page).to have_content multichoice.question
       expect(page).to have_content option.content
       expect(page).to have_content option2.content
       expect(page).to have_content option3.content
+    end
+
+  end
+
+  describe "if user is signed in as a teacher" do
+
+    let!(:user){FactoryGirl.create(:user)}
+
+    before :each do
+      sign_in(username:"Testipoika", password:"Salainen1")
+
+      visit root_path
+
+      click_button('Lihanautakuolemat')
+      click_link('Toimenpiteet')
+
+    end
+
+    it "user should be able to preview the content of a text task" do
+      click_button(task.name)
+      click_link('Esikatsele')
+      expect(page).to have_content task_text.content
+      expect(page).to have_content 'Toimenpide: ' + task.name
+      expect(page).not_to have_button 'Jatka'
+    end
+
+    it "user should be able to preview the question, answer options and explanations for a multichoice task" do
+      click_button(multichoice_task.name)
+      click_link('Esikatsele')
+
+      expect(page).to have_content multichoice.question
+      expect(page).to have_content option.content
+      expect(page).to have_content option2.content
+      expect(page).to have_content option3.content
+
+      expect(page).to have_content option.explanation
+      expect(page).to have_content option2.explanation
+      expect(page).to have_content option3.explanation
+
+      expect(page).not_to have_button 'Tarkista'
     end
 
   end
