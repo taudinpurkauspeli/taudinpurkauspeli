46120fb7eb61865cf0993d95c7144df8272638c9
Eveliina <pevetiainen.ep@hotmail.com>
2015-03-21 01:21:19 +0200

    Started adding new tests

--- spec/features/edit_exercise_page_spec.rb
+++ spec/features/edit_exercise_page_spec.rb
@@ -12,7 +12,6 @@ describe "Edit Exercise page", js:true do
 			sign_in(username:"Testipoika", password:"Salainen1")
 		end
 
-
 		it "user should be able to edit exercise" do
 			visit edit_exercise_path(exercise)
 
@@ -20,9 +19,12 @@ describe "Edit Exercise page", js:true do
 			fill_in('exercise_anamnesis', with: "Mitä kanoille on tapahtunut??")
 
 			click_button('Tallenna')
+			wait_for_ajax
 
 			expect(current_path).to eq(exercise_path(exercise))
 			expect(page).to have_content 'Broilerimysteeri'
+			expect(Exercise.first.name).to eq('Broilerimysteeri')
+			expect(Exercise.first.anamnesis).to eq('Mitä kanoille on tapahtunut??')
 		end
 
 		it "user should not be able to edit exercise wrong" do
@@ -32,38 +34,31 @@ describe "Edit Exercise page", js:true do
 			fill_in('exercise_anamnesis', with: "Mitä kanoille on tapahtunut??")
 
 			click_button('Tallenna')
+			wait_for_ajax
 
 			expect(current_path).to eq(exercise_path(exercise))
 			expect(page).to have_content 'estivät tallennuksen'
 		end
 
-		
-
-
 	end
 
-	describe "if user is signed in as normal user" do
+	describe "if user is signed in as a student" do
 
-		let!(:user){FactoryGirl.create(:user, admin: false)}
+		let!(:user){FactoryGirl.create(:student)}
 		let!(:exercise){FactoryGirl.create(:exercise)}
 
 		before :each do
 			sign_in(username:"Testipoika", password:"Salainen1")
 		end
 
-
 		it "user should not be able to visit edit exercise page" do
 			visit edit_exercise_path(exercise)
 
 			expect(current_path).to eq(signin_path)
-			
 		end
 
-		
 	end
 
-
-
-
 end
+
 =end
--- spec/features/new_task_multichoice_spec.rb
+++ spec/features/new_task_multichoice_spec.rb
@@ -1,4 +1,3 @@
-=begin
 require 'rails_helper'
 
 describe "New Task page", js:true do
@@ -11,47 +10,69 @@ describe "New Task page", js:true do
     before :each do
       sign_in(username:"Testipoika", password:"Salainen1")
       click_button(exercise.name)
+      wait_for_ajax
     end
-
-
 
     it "user should be able to create a new task with a multichoice subtask" do
-      visit new_task_path
+      click_link('Toimenpiteet')
+      wait_for_ajax
+
+      click_button('+ Luo uusi toimenpide')
+      wait_for_ajax
+
       fill_in('task_name', with: "Kysymyksiä asiakkaalle")
-      click_button('Tallenna')
-      click_button('Luo uusi monivalinta-alitoimenpide')
+
+      expect{
+        click_button('Tallenna')
+        wait_for_ajax
+      }.to change(Task, :count).by(1)
+
+      click_button('+ Luo uusi monivalinta-alitoimenpide')
+      wait_for_ajax
 
       fill_in('multichoice_question', with: "Mitä kysyt asiakkaalta:")
-      click_button('Tallenna kysymys')
 
-      expect(page).to have_content 'Kysymys lisättiin onnistuneesti.'
-      expect(Task.count).to eq(1)
-      expect(Multichoice.count).to eq(1)
+      expect{
+        click_button('Tallenna kysymys')
+        wait_for_ajax
+      }.to change(Multichoice, :count).by(1)
+
+      #expect(page).to have_content 'Kysymys lisättiin onnistuneesti.'
       expect(Subtask.count).to eq(1)
       expect(Multichoice.first.question).to eq("Mitä kysyt asiakkaalta:")
+      expect(Multichoice.first.subtask.task.name).to eq("Kysymyksiä asiakkaalle")
     end
 
 
-    it "user should be able to update the content of a multichoice subtask" do
-      visit new_task_path
-      fill_in('task_name', with: "Kysymyksiä asiakkaalle")
-      click_button('Tallenna')
-      click_button('Luo uusi monivalinta-alitoimenpide')
 
-      fill_in('multichoice_question', with: "Mitä kysyt asiakkaalta:")
-      click_button('Tallenna kysymys')
+=begin
+    it "user should be able to update the content of a multichoice subtask" do
+
+      task = FactoryGirl.create(:task)
+      FactoryGirl.create(:subtask)
+      multichoice = FactoryGirl.create(:multichoice)
+
+      click_link('Toimenpiteet')
+      wait_for_ajax
+
+      click_button(task.name)
+      wait_for_ajax
+
+      click_button(multichoice.name)
+      wait_for_ajax
 
       expect(page).to have_content 'Muokkaa monivalintakysymystä'
       fill_in('multichoice_question', with: "Useita kysymyksiä asiakkaalle:")
+
       click_button('Tallenna kysymys')
+      wait_for_ajax
 
-      expect(current_path).to eq(edit_multichoice_path(1))
-      expect(Task.count).to eq(1)
-      expect(Multichoice.count).to eq(1)
-      expect(Subtask.count).to eq(1)
+      expect(current_path).to eq(exercise_path(Exercise.first))
       expect(Multichoice.first.question).to eq("Useita kysymyksiä asiakkaalle:")
-
     end
+=end
+
+=begin
 
     it "user should not be able to create a new multichoice subtask without question" do
       visit new_task_path
@@ -82,8 +103,8 @@ describe "New Task page", js:true do
       expect(page).to have_content 'Seuraavat virheet estivät tallennuksen:'
       expect(Task.first.name).to eq("Kysymyksiä asiakkaalle")
     end
+=end
 
   end
 
 end
-=end
