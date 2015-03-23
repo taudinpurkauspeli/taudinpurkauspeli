a10d209ca4b4cec9ee812ab043a647f263377f0f
41407 <jimi.welling@cs.helsinki.fi>
2015-03-23 16:26:13 +0200

    Create anamnesis:Task on ex create

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -31,6 +31,7 @@
     @exercise = Exercise.new(exercise_params)
     respond_to do |format|
       if @exercise.save
+        @exercise.tasks.create(name:"Anamneesi", level:0)
         format.html { redirect_to @exercise, notice: 'Casen luominen onnistui!' }
         format.json { render :show, status: :created, location: @exercise }
       else
