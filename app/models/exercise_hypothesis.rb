d688db755dea3605b0905fe42b534d012b904fb9
41407 <jimi.welling@cs.helsinki.fi>
2015-03-23 16:34:46 +0200

    Fix student to always be able to check hyps if hyp.preq.level < 1

--- app/models/exercise_hypothesis.rb
+++ app/models/exercise_hypothesis.rb
@@ -16,6 +16,7 @@ class ExerciseHypothesis < ActiveRecord::Base
   end
 
   def user_meets_requirements (user)
+    return true if get_prerequisite.level < 1
     if(user.completed_tasks.where(task_id: task_id).empty?)
       return false
     end
