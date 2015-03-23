0287cef57b9f9a240b3b60545a9ae7a2f304e2a0
dani nu <dani.nummikoski@gmail.com>
2015-03-23 16:00:58 +0200

    update models

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,6 +2,5 @@ class Interview < ActiveRecord::Base
 	validates :title, presence: true
 
 	belongs_to :subtask
-	has_many :question_groups, dependent: :destroy
-	has_many :questions, through: :question_groups
+	has_many :questions, dependent: :destroy
 end
