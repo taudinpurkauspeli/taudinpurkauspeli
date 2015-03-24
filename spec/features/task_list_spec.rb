d99a1bbba2e4a471b69df764bf5775d80ddef3bd
41407 <jimi.welling@cs.helsinki.fi>
2015-03-24 15:08:47 +0200

    Fix rest of the specs

--- spec/features/task_list_spec.rb
+++ spec/features/task_list_spec.rb
@@ -5,24 +5,24 @@ describe "Task list page" do
   let!(:exercise){FactoryGirl.create(:exercise)}
   let!(:exercise2){FactoryGirl.create(:exercise, name: "Kanakuolemat")}
 
-  let!(:task){FactoryGirl.create(:task)}
-  let!(:subtask){FactoryGirl.create(:subtask)}
-  let!(:task_text){FactoryGirl.create(:task_text)}
+  let!(:task){FactoryGirl.create(:task, exercise:exercise, level:1)}
+  let!(:subtask){FactoryGirl.create(:subtask, task:task)}
+  let!(:task_text){FactoryGirl.create(:task_text, subtask:subtask)}
 
 
-  let!(:task2_1){FactoryGirl.create(:task, name: "Hoida", level: 2)}
-  let!(:subtask2){FactoryGirl.create(:subtask, task_id: 2)}
-  let!(:task_text2){FactoryGirl.create(:task_text, subtask_id:2, content:"Lääkäri hoitaa")}
+  let!(:task2_1){FactoryGirl.create(:task, exercise:exercise, name: "Hoida", level: 2)}
+  let!(:subtask2){FactoryGirl.create(:subtask, task:task2_1)}
+  let!(:task_text2){FactoryGirl.create(:task_text, subtask:subtask2, content:"Lääkäri hoitaa")}
 
 
-  let!(:task2_2){FactoryGirl.create(:task, name: "Hoida eläintä", level: 2)}
-  let!(:task_text3){FactoryGirl.create(:task_text, subtask_id:3, content:"Lääkäri hoitaa eläintä")}
-  let!(:subtask3){FactoryGirl.create(:subtask, task_id: 3)}
+  let!(:task2_2){FactoryGirl.create(:task, exercise:exercise, name: "Hoida eläintä", level: 2)}
+  let!(:task_text3){FactoryGirl.create(:task_text, subtask:subtask3, content:"Lääkäri hoitaa eläintä")}
+  let!(:subtask3){FactoryGirl.create(:subtask, task:task2_2)}
 
 
-  let!(:task3){FactoryGirl.create(:task, exercise_id: 2, name: "Soita asiakkaalle uudestaan")}
+  let!(:task3){FactoryGirl.create(:task, exercise:exercise2, name: "Soita asiakkaalle uudestaan")}
 
-  let!(:task4){FactoryGirl.create(:task, name: "Soita asiakkaalle eläimestä", level: 3)}
+  let!(:task4){FactoryGirl.create(:task, exercise:exercise, name: "Soita asiakkaalle eläimestä", level: 3)}
 
   describe "if user is signed in as student" do
     let!(:user){FactoryGirl.create(:student)}
@@ -61,7 +61,7 @@ describe "Task list page" do
 
 
       it "user should be able to do the tasks of same level in any order (case a)" do
-        FactoryGirl.create(:completed_task)
+        FactoryGirl.create(:completed_task, task:task, user:user)
         click_button(task2_1.name)
         click_button('Jatka')
         click_button(task2_2.name)
@@ -72,7 +72,7 @@ describe "Task list page" do
       end
 
       it "user should be able to do the tasks of same level in any order (case b)" do
-        FactoryGirl.create(:completed_task)
+        FactoryGirl.create(:completed_task, task:task, user:user)
         click_button(task2_2.name)
         click_button('Jatka')
         click_button(task2_1.name)
@@ -92,6 +92,9 @@ describe "Task list page" do
   end
 
   describe "if user is signed in as teacher" do
+
+    # CODE HARD
+
     let!(:user){FactoryGirl.create(:user)}
 
     before :each do
@@ -107,69 +110,69 @@ describe "Task list page" do
       expect(task2_1.level).to eq(2)
       expect(task2_2.level).to eq(2)
 
-      click_on("tasks/5/up")
+      click_on("tasks/7/up")
       expect(Task.last.level).to eq(2)
 
-      expect(Task.first.level).to eq(1)
-      expect(Task.find(2).level).to eq(2)
-      expect(Task.find(3).level).to eq(2)
+      expect(Task.where(level:1...9999).first.level).to eq(1)
+      expect(Task.find(4).level).to eq(2)
+      expect(Task.find(5).level).to eq(2)
     end
 
 
     it "he should be able to move task one level up if it has siblings and children" do
-      click_on("tasks/5/up")
-      click_on("tasks/5/up")
+      click_on("tasks/7/up")
+      click_on("tasks/7/up")
       expect(Task.last.level).to eq(2)
 
-      expect(Task.first.level).to eq(1)
-      expect(Task.find(2).level).to eq(3)
-      expect(Task.find(3).level).to eq(3)
+      expect(Task.where(level:1...9999).first.level).to eq(1)
+      expect(Task.find(4).level).to eq(3)
+      expect(Task.find(5).level).to eq(3)
     end
 
     it "he should be able to move task one level up if it has children but no siblings" do
-      click_on("tasks/5/up")
-      click_on("tasks/5/up")
-      click_on("tasks/5/up")
+      click_on("tasks/7/up")
+      click_on("tasks/7/up")
+      click_on("tasks/7/up")
       expect(Task.last.level).to eq(1)
 
-      expect(Task.first.level).to eq(1)
-      expect(Task.find(2).level).to eq(2)
-      expect(Task.find(3).level).to eq(2)
+      expect(Task.where(level:1...9999).first.level).to eq(1)
+      expect(Task.find(4).level).to eq(2)
+      expect(Task.find(5).level).to eq(2)
     end
 
     it "he should be able to move task one level down if it has children and siblings" do
-      click_on("tasks/5/up")
-      click_on("tasks/5/up")
-      click_on("tasks/5/up")
-      click_on("tasks/5/down")
+      click_on("tasks/7/up")
+      click_on("tasks/7/up")
+      click_on("tasks/7/up")
+      click_on("tasks/7/down")
       expect(Task.last.level).to eq(2)
 
-      expect(Task.first.level).to eq(1)
-      expect(Task.find(2).level).to eq(3)
-      expect(Task.find(3).level).to eq(3)
+      expect(Task.where(level:1...9999).first.level).to eq(1)
+      expect(Task.find(4).level).to eq(3)
+      expect(Task.find(5).level).to eq(3)
     end
 
     it "he should be able to move task one level down if it has children and no siblings" do
 
-      click_on("tasks/5/up")
-      click_on("tasks/5/up")
-      click_on("tasks/5/down")
+      click_on("tasks/7/up")
+      click_on("tasks/7/up")
+      click_on("tasks/7/down")
       expect(Task.last.level).to eq(2)
 
-      expect(Task.first.level).to eq(1)
-      expect(Task.find(2).level).to eq(2)
-      expect(Task.find(3).level).to eq(2)
+      expect(Task.where(level:1...9999).first.level).to eq(1)
+      expect(Task.find(4).level).to eq(2)
+      expect(Task.find(5).level).to eq(2)
     end
 
     it "he should be able to move task one level down if it has siblings and no children" do
 
-      click_on("tasks/5/up")
-      click_on("tasks/5/down")
+      click_on("tasks/7/up")
+      click_on("tasks/7/down")
       expect(Task.last.level).to eq(3)
 
-      expect(Task.first.level).to eq(1)
-      expect(Task.find(2).level).to eq(2)
-      expect(Task.find(3).level).to eq(2)
+      expect(Task.where(level:1...9999).first.level).to eq(1)
+      expect(Task.find(4).level).to eq(2)
+      expect(Task.find(5).level).to eq(2)
     end
 
   end
