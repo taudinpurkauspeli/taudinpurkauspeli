e30c9ff95f4a13cd2822e07397ba34b14b72bf91
41407 <jimi.welling@cs.helsinki.fi>
2015-03-26 14:15:35 +0200

    Move and improve user.has_completed?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -67,6 +67,25 @@ RSpec.describe User, :type => :model do
     expect(user.get_number_of_tasks_by_level(exercise, task.level)).to eq(1)
   end
 
+  describe "has_completed" do
+    let!(:user){FactoryGirl.create(:user)}
+    let!(:task){FactoryGirl.create(:task, exercise_id:1)}
+    let!(:subtask){FactoryGirl.create(:subtask, task:task)}
+
+    before(:each) do
+      user.complete_subtask(subtask)
+      user.complete_task(task)
+    end
+
+    it "finds completed task" do
+      expect(user.has_completed?(task)).to eq(true)
+    end
+
+    it "finds completed subtask" do
+      expect(user.has_completed?(subtask)).to eq(true)
+    end
+  end
+
   describe "completed" do
     let!(:user){FactoryGirl.create(:user)}
     let!(:task){FactoryGirl.create(:task, exercise_id:1)}
