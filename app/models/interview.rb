4b773d0b63612ae41f6b2223c4efc683cd48a970
dani nu <dani.nummikoski@gmail.com>
2015-03-27 15:07:01 +0200

    questions by group

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -3,6 +3,7 @@ class Interview < ActiveRecord::Base
 
 	belongs_to :subtask
 	has_many :questions, dependent: :destroy
+	has_many :question_groups, through: :questions
 
 	def user_has_asked_all_required(user)
     user.questions
