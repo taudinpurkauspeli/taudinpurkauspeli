3d2c5ca5537044595a98bf72c936c804400e2e3f
Eveliina <pevetiainen.ep@hotmail.com>
2015-04-07 16:03:59 +0300

    Exercise and exhyp

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,11 +1,17 @@
 class Exercise < ActiveRecord::Base
-	validates :name, presence: true, uniqueness: true, length: {minimum: 2}
+  validates :name, presence: true, length: {minimum: 2}
   after_create :create_anamnesis
-	has_many :tasks
-  
+
+  has_many :tasks
+  has_many :exercise_hypotheses, -> { includes(:hypothesis).order('hypotheses.name')}, dependent: :destroy
   has_many :hypotheses, through: :exercise_hypotheses
-	has_many :exercise_hypotheses, -> { includes(:hypothesis).order('hypotheses.name')}, dependent: :destroy
-	has_many :checked_hypotheses,  -> { includes(:hypothesis).order('hypotheses.name')}, through: :exercise_hypotheses
+  has_many :checked_hypotheses,  -> { includes(:hypothesis).order('hypotheses.name')}, through: :exercise_hypotheses
+
+  amoeba do
+    enable
+    include_association :exercise_hypotheses
+    include_association :tasks
+  end
 
   def get_hypotheses
     return exercise_hypotheses.group_by{|exhyp| exhyp.hypothesis.hypothesis_group_id}
