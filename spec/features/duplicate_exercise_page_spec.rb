cb10fdee8544c1a470861c3b7ee327a6e03ad5cf
Eveliina <pevetiainen.ep@hotmail.com>
2015-04-09 18:03:46 +0300

    Fix tests

--- spec/features/duplicate_exercise_page_spec.rb
+++ spec/features/duplicate_exercise_page_spec.rb
@@ -23,7 +23,7 @@ describe "Duplicate exercise page" do
   let!(:multichoice_subtask){FactoryGirl.create(:subtask, task_id:multichoice_task.id)}
   let!(:multichoice){FactoryGirl.create(:multichoice, subtask_id:multichoice_subtask.id)}
   let!(:option){FactoryGirl.create(:option, multichoice_id:multichoice.id)}
-  let!(:option2){FactoryGirl.create(:option, multichoice_id:multichoice.id, content: "Ei tykkää", is_correct_answer: false, explanation: "Ei oikea vastaus")}
+  let!(:option2){FactoryGirl.create(:option, multichoice_id:multichoice.id, content: "Ei tykkää", is_correct_answer: "wrong", explanation: "Ei oikea vastaus")}
   let!(:option3){FactoryGirl.create(:option, multichoice_id:multichoice.id, content: "Ehkä tykkää", explanation: "Melkein oikea vastaus")}
 
   #Interview task
