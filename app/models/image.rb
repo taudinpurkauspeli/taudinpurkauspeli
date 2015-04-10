a0c2347aae40106c72552175c6c9ed96993cba2f
41407 <jimi.welling@cs.helsinki.fi>
2015-04-10 13:04:08 +0300

    Add image association to Exercise

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,10 @@
 class Image < ActiveRecord::Base
+
+  has_attached_file :picture, styles: {
+    original: '1070>',
+    thumb: '100x100#'
+  }
+
+      # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
