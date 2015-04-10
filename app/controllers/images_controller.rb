1c506580435f4e35a747e3ad31f3eaa88680ed8e
41407 <jimi.welling@cs.helsinki.fi>
2015-04-10 13:23:30 +0300

    Fix image controller to permit :name

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -69,6 +69,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-    params.require(:image).permit(:picture)
+    params.require(:image).permit(:name, :picture)
     end
 end
