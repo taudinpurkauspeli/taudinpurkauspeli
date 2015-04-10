12fcd23ed19f3dff86a0c052cde55c6bab6da89e
41407 <jimi.welling@cs.helsinki.fi>
2015-04-10 14:38:06 +0300

    Fix task text show, form for image

--- app/controllers/task_texts_controller.rb
+++ app/controllers/task_texts_controller.rb
@@ -71,6 +71,6 @@ class TaskTextsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def task_text_params
-    params.require(:task_text).permit(:content, :picture)
+    params.require(:task_text).permit(:content, :image_id)
   end
 end
