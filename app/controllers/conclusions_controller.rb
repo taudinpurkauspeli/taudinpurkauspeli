42e2ae665b6b4d35c7c5f95d488098f7266adfa0
dani nu <dani.nummikoski@gmail.com>
2015-04-15 15:48:51 +0300

    conclusion create + edit view

--- app/controllers/conclusions_controller.rb
+++ app/controllers/conclusions_controller.rb
@@ -15,6 +15,26 @@ class ConclusionsController < ApplicationController
 		set_view_layout
 	end
 
+	def create
+		@task = Task.find(session[:task_id])
+
+		# This can be done for each different type of subtask in their respective controllers
+		subtask = @task.subtasks.build
+		@conclusion = subtask.build_conclusion(title:conclusion_params[:title], content:conclusion_params[:content])
+
+		respond_to do |format|
+			if @conclusion.save
+				subtask.save
+				format.html { redirect_to edit_conclusion_path(@conclusion.id, :layout => get_layout), notice: 'Päätöstoimenpide lisättiin onnistuneesti.' }
+				#format.json { render :show, status: :created, location: @multichoice }
+			else
+				## TODO redirect task show
+				format.html { redirect_to new_conclusion_path(:layout => get_layout), alert: 'Päätöstoimenpiteen lisääminen epäonnistui.' }
+				format.json { render json: @conclusion.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
 	def destroy
 		@conclusion.destroy
 		respond_to do |format|
