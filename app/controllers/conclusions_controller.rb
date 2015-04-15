e0b5055722d6ca06c717fcb0c5148f59792f5ffb
dani nu <dani.nummikoski@gmail.com>
2015-04-15 15:10:28 +0300

    conclusion controller show, new, edit

--- app/controllers/conclusions_controller.rb
+++ app/controllers/conclusions_controller.rb
@@ -1,3 +1,17 @@
 class ConclusionsController < ApplicationController
 
+	def show
+		set_view_layout
+	end
+
+	def new
+		@conclusion = Conclusion.new
+		set_view_layout
+	end
+
+	# GET /conclusions/1/edit
+	def edit
+		set_view_layout
+	end
+
 end
