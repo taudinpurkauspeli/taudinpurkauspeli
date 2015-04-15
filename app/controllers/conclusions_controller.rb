1a99d1e90d05078b60b3a97dfbad5a8d1a1e4737
dani nu <dani.nummikoski@gmail.com>
2015-04-15 15:36:47 +0300

    conclusion destroy

--- app/controllers/conclusions_controller.rb
+++ app/controllers/conclusions_controller.rb
@@ -1,4 +1,5 @@
 class ConclusionsController < ApplicationController
+	before_action :ensure_user_is_logged_in
 
 	def show
 		set_view_layout
@@ -12,6 +13,19 @@ class ConclusionsController < ApplicationController
 	# GET /conclusions/1/edit
 	def edit
 		set_view_layout
+	end
+
+	def destroy
+		@conclusion.destroy
+		respond_to do |format|
+			format.html { redirect_to tasks_path(:layout => get_layout), notice: 'Taskin poisto onnistui!' }
+			format.json { head :no_content }
+		end
+	end
+
+	private
+	def conclusion_params
+		params.require(:conclusion).permit(:title, :content)
 	end
 
 end
