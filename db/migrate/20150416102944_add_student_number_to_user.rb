8dbfcc48d1ce7007acdf51a04e2818c81aec86f6
Tarja <tarjako@hotmail.com>
2015-04-16 13:49:46 +0300

    Added fields to user model

---
+++ db/migrate/20150416102944_add_student_number_to_user.rb
@@ -0,0 +1,5 @@
+class AddStudentNumberToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :student_number, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415122320) do
+ActiveRecord::Schema.define(version: 20150416103445) do
 
   create_table "asked_questions", force: :cascade do |t|
     t.datetime "created_at",  null: false
