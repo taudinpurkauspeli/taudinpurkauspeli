fad96a1f959ade577581b25addd0e17e646f4517
dani nu <dani.nummikoski@gmail.com>
2015-04-16 17:37:02 +0300

    reindent everything

--- app/models/conclusion.rb
+++ app/models/conclusion.rb
@@ -2,9 +2,7 @@ class Conclusion < ActiveRecord::Base
 	belongs_to :subtask
 	belongs_to :exercise_hypothesis
 
-
 	def user_answered_correctly?(user, final_conclusion)
-		puts "USER ANSWARED: " + final_conclusion + "OIKEA VASTAUS: " + exercise_hypothesis.id.to_s
 		if final_conclusion.to_i == exercise_hypothesis.id
 			user.complete_subtask(subtask)
 			return true
