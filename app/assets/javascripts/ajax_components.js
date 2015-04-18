e28fd0651c7153232ddcebe5296eb2787273704a
Eveliina <pevetiainen.ep@hotmail.com>
2015-04-18 17:41:56 +0300

    Wait for ckeditor script

--- app/assets/javascripts/ajax_components.js
+++ app/assets/javascripts/ajax_components.js
@@ -184,3 +184,19 @@ function showTab(tabElementSelector){
  });
  }
  */
+
+/**
+ * Function for testing ckeditor text editors.
+ *
+ * Allows tests to wait until ckeditor has been loaded.
+ *
+ */
+
+function isCkeditorLoaded(instance_selector) {
+    // instance_selector is e.g. 'template_html'
+    var status;
+    if (window.CKEDITOR && CKEDITOR.instances && CKEDITOR.instances[instance_selector]) {
+        status = CKEDITOR.instances[instance_selector].status;
+    }
+    return status === 'ready';
+};
