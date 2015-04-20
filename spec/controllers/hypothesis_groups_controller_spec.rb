8f9bd15a4e8d8a1d40e32926dd7378850d7852a0
dani nu <dani.nummikoski@gmail.com>
2015-04-20 14:46:04 +0300

    test question_groups_controller

--- spec/controllers/hypothesis_groups_controller_spec.rb
+++ spec/controllers/hypothesis_groups_controller_spec.rb
@@ -3,15 +3,12 @@ require 'rails_helper'
 RSpec.describe HypothesisGroupsController, :type => :controller do
 
   let!(:user){FactoryGirl.create(:user)}
+  let!(:exercise){FactoryGirl.create(:exercise)}
   # This should return the minimal set of attributes required to create a valid
   # HypothesisGroup. As you add validations to HypothesisGroup, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
     {name: "Bakteeritauti"}
-  }
-
-  let!(:exercise){
-    FactoryGirl.create(:exercise)
   }
 
 
@@ -23,139 +20,51 @@ RSpec.describe HypothesisGroupsController, :type => :controller do
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # HypothesesController. Be sure to keep this updated too.
-  let(:valid_session) { {
-      user_id: 1
-  } }
-
-=begin
-  describe "GET index" do
-    it "assigns all hypothesis groups as @hypothesis_group" do
-      hypothesis_goup = HypothesisGroup.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:hypothesis_group)).to eq([hypothesis_group])
-    end
-  end
-
-  describe "GET show" do
-    it "assigns the requested hypothesis group as @hypothesis_group" do
-      hypothesis_group = HypothesisGroup.create! valid_attributes
-      get :show, {:id => hypothesis_group.to_param}, valid_session
-      expect(assigns(:hypothesis_group)).to eq(hypothesis_group)
-    end
-  end
-=end
-
-=begin
-  describe "GET new" do
-    it "assigns a new hypothesis group as @hypothesis_group" do
-      get :new, {}, valid_session
-      expect(assigns(:hypothesis_group)).to be_a_new(HypothesisGroup)
-    end
-  end
-=end
-
-=begin
-  describe "GET edit" do
-    it "assigns the requested hypothesis group as @hypothesis_group" do
-      hypothesis_group = HypothesisGroup.create! valid_attributes
-      get :edit, {:id => hypothesis_group.to_param}, valid_session
-      expect(assigns(:hypothesis_group)).to eq(hypothesis_group)
-    end
-  end
-=end
+  let(:valid_session) { {      user_id: 1} }
 
   describe "POST create" do
     describe "with valid params" do
       it "creates a new HypothesisGroup" do
         expect {
           post :create, {:hypothesis_group => valid_attributes}, valid_session
-        }.to change(HypothesisGroup, :count).by(1)
+          }.to change(HypothesisGroup, :count).by(1)
+        end
+
+        it "assigns a newly created hypothesis group as @hypothesis_group" do
+          post :create, {:hypothesis_group => valid_attributes}, valid_session
+          expect(assigns(:hypothesis_group)).to be_a(HypothesisGroup)
+          expect(assigns(:hypothesis_group)).to be_persisted
+        end
+
+        it "redirects to the created hypothesis group" do
+          post :create, {:hypothesis_group => valid_attributes}, valid_session
+          expect(response).to redirect_to(hypotheses_path(:layout => true))
+        end
       end
 
-      it "assigns a newly created hypothesis group as @hypothesis_group" do
-        post :create, {:hypothesis_group => valid_attributes}, valid_session
-        expect(assigns(:hypothesis_group)).to be_a(HypothesisGroup)
-        expect(assigns(:hypothesis_group)).to be_persisted
-      end
 
-      it "redirects to the created hypothesis group" do
-        post :create, {:hypothesis_group => valid_attributes}, valid_session
-        expect(response).to redirect_to(hypotheses_path(:layout => true))
+
+      describe "with invalid params" do
+        it "assigns a newly created but unsaved hypothesis group as @hypothesis_group" do
+          post :create, {:hypothesis_group => invalid_attributes}, valid_session
+          expect(assigns(:hypothesis_group)).to be_a_new(HypothesisGroup)
+        end
       end
     end
-
-
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved hypothesis group as @hypothesis_group" do
-        post :create, {:hypothesis_group => invalid_attributes}, valid_session
-        expect(assigns(:hypothesis_group)).to be_a_new(HypothesisGroup)
-      end
-    end
-
-
-  end
-
-=begin
-
-  describe "PUT update" do
-    describe "with valid params" do
-      let(:new_attributes) {
-        {name: "Virustauti"}
-      }
-
-      it "updates the requested hypothesis group" do
+    
+    describe "DELETE destroy" do
+      it "destroys the requested hypothesis group" do
         hypothesis_group = HypothesisGroup.create! valid_attributes
-        put :update, {:id => hypothesis_group.to_param, :hypothesis_group => new_attributes}, valid_session
-        hypothesis_group.reload
-        expect(hypothesis_group.name).to eq("Virustauti")
+        expect {
+          delete :destroy, {:id => hypothesis_group.to_param}, valid_session
+          }.to change(HypothesisGroup, :count).by(-1)
+        end
+
+        it "redirects to the hypotheses list" do
+          hypothesis_group = HypothesisGroup.create! valid_attributes
+          delete :destroy, {:id => hypothesis_group.to_param}, valid_session
+          expect(response).to redirect_to(hypotheses_path(:layout => true))
+        end
       end
 
-      it "assigns the requested hypothesis group as @hypothesis_group" do
-        hypothesis_group = HypothesisGroup.create! valid_attributes
-        put :update, {:id => hypothesis_group.to_param, :hypothesis_group => valid_attributes}, valid_session
-        expect(assigns(:hypothesis_group)).to eq(hypothesis_group)
-      end
-
-      it "redirects to the hypothesis group" do
-        hypothesis_group = HypothesisGroup.create! valid_attributes
-        put :update, {:id => hypothesis_group.to_param, :hypothesis_group => valid_attributes}, valid_session
-        expect(response).to redirect_to(hypothesis_group)
-      end
     end
-=end
-
-=begin
-    describe "with invalid params" do
-      it "assigns the hypothesis group as @hypothesis_group" do
-        hypothesis_group = HypothesisGroup.create! valid_attributes
-        put :update, {:id => hypothesis_group.to_param, :hypothesis_group => invalid_attributes}, valid_session
-        expect(assigns(:hypothesis_group)).to eq(hypothesis_group)
-      end
-
-      it "re-renders the 'edit' template" do
-        hypothesis_group = HypothesisGroup.create! valid_attributes
-        put :update, {:id => hypothesis_group.to_param, :hypothesis_group => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-=end
-  #end
-
-
-  describe "DELETE destroy" do
-    it "destroys the requested hypothesis group" do
-      hypothesis_group = HypothesisGroup.create! valid_attributes
-      expect {
-        delete :destroy, {:id => hypothesis_group.to_param}, valid_session
-      }.to change(HypothesisGroup, :count).by(-1)
-    end
-
-    it "redirects to the hypotheses list" do
-      hypothesis_group = HypothesisGroup.create! valid_attributes
-      delete :destroy, {:id => hypothesis_group.to_param}, valid_session
-      expect(response).to redirect_to(hypotheses_path(:layout => true))
-    end
-  end
-
-end
--- spec/controllers/options_controller_spec.rb
+++ spec/controllers/options_controller_spec.rb
@@ -3,28 +3,29 @@ require 'rails_helper'
 RSpec.describe OptionsController, :type => :controller do
 
 	let!(:user){FactoryGirl.create(:user)}
-	let!(:task){FactoryGirl.create(:task)}
-	let!(:multichoice){FactoryGirl.create(:multichoice)}
+	let!(:exercise){FactoryGirl.create(:exercise)}
+	let!(:task){FactoryGirl.create(:task, exercise:exercise)}
+	let!(:subtask){FactoryGirl.create(:subtask, task:task)}
+	let!(:multichoice){FactoryGirl.create(:multichoice, subtask:subtask)}
+
+	let(:valid_session) {{ user_id: 1, task_id: task.id}}
 
 	let(:valid_attributes) {{content: "4 jalkaa", explanation: "Normaalilla lehmällä on kaksi etujalkaa ja kaksi takajalkaa",
-													 is_correct_answer: "required", multichoice_id: 1
+		is_correct_answer: "required", multichoice_id: multichoice.id
 	}}
 
-	let(:invalid_attributes) {{content: nil, explanation: nil, multichoice_id: 1}}
+	let(:invalid_attributes) {{content: nil, explanation: nil, multichoice_id: multichoice.id }}
 
-	let(:valid_session) { {
-			user_id: 1, task_id: 1
-	} }
+	let(:valid_session) { {	user_id: 1, task_id: task.id} }
 
 	describe "POST create" do
 		describe "with valid params" do
 			it "creates a new Option" do
 				expect {
 					post :create, {:option => valid_attributes}, valid_session
-				}.to change(Option, :count).by(1)
+					}.to change(Option, :count).by(1)
 			end
 		end
-
 	end
 
 	describe "POST create" do
@@ -32,10 +33,9 @@ RSpec.describe OptionsController, :type => :controller do
 			it "does not create a new Option" do
 				expect {
 					post :create, {:option => invalid_attributes}, valid_session
-				}.not_to change(Option, :count)
+					}.not_to change(Option, :count)
 			end
 		end
-
 	end
 
 	describe "PUT update" do
@@ -62,9 +62,7 @@ RSpec.describe OptionsController, :type => :controller do
 				put :update, {:id => option.to_param, :option => invalid_attributes}, valid_session
 				expect(assigns(:option)).to eq(option)
 			end
-
 		end
-
 	end
 
 	describe "DELETE destroy" do
@@ -72,14 +70,14 @@ RSpec.describe OptionsController, :type => :controller do
 			option = Option.create! valid_attributes
 			expect {
 				delete :destroy, {:id => option.to_param}, valid_session
-			}.to change(Option, :count).by(-1)
+				}.to change(Option, :count).by(-1)
 		end
 
 		it "redirects to the multichoice edit" do
 			option = Option.create! valid_attributes
 			delete :destroy, {:id => option.to_param}, valid_session
-			expect(response).to redirect_to(edit_multichoice_path(:layout => true))
+			expect(response).to redirect_to(edit_multichoice_path(multichoice.id, :layout => true))
 		end
 	end
-
 end
+
