05420d4ba49ae75514f0860b07681cf13f394a52
dani nu <dani.nummikoski@gmail.com>
2015-04-21 13:07:48 +0300

    delete order from user model

--- db/schema.rb
+++ db/schema.rb
@@ -130,8 +130,8 @@ ActiveRecord::Schema.define(version: 20150416104311) do
     t.string   "explanation"
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
-    t.integer  "image_id"
     t.integer  "is_correct_answer", default: 0
+    t.integer  "image_id"
   end
 
   create_table "question_groups", force: :cascade do |t|
