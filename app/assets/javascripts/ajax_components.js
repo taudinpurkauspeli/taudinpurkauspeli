7cf7e9b7d3692c3c4d74379f23d24cede73e8b91
Jouko <jouko.niinimaki@cs.helsinki.fi>
2015-04-22 12:34:09 +0300

    notices z index bigger and some refactoring

--- app/assets/javascripts/ajax_components.js
+++ app/assets/javascripts/ajax_components.js
@@ -52,11 +52,7 @@ function setAjaxSubmits(formsSelector, containerElementSelector, type, submitCal
 
 }
 
-function CKupdate(){
-    for ( instance in CKEDITOR.instances ){
-        CKEDITOR.instances[instance].updateElement();
-    }
-}
+
 
 /**
  *
@@ -184,19 +180,3 @@ function showTab(tabElementSelector){
  });
  }
  */
-
-/**
- * Function for testing ckeditor text editors.
- *
- * Allows tests to wait until ckeditor has been loaded.
- *
- */
-
-function isCkeditorLoaded(instance_selector) {
-    // instance_selector is e.g. 'template_html'
-    var status;
-    if (window.CKEDITOR && CKEDITOR.instances && CKEDITOR.instances[instance_selector]) {
-        status = CKEDITOR.instances[instance_selector].status;
-    }
-    return status === 'ready';
-};
