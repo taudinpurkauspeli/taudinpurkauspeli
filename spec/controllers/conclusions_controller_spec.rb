525931d8243ddd643d1c9c0dc6f7b3ac57b60763
leijona <dani.nummikoski@gmail.com>
2015-04-24 17:40:45 +0300

    check answers test

--- spec/controllers/conclusions_controller_spec.rb
+++ spec/controllers/conclusions_controller_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe ConclusionsController, :type => :controller do
 	let!(:subtask){FactoryGirl.create(:subtask, task:task)}
 
 	let!(:hypothesis){FactoryGirl.create(:hypothesis)}
-	let!(:exercise_hypothesis){FactoryGirl.create(:exercise_hypothesis,)}
+	let!(:exercise_hypothesis){FactoryGirl.create(:exercise_hypothesis, hypothesis:hypothesis, exercise:exercise)}
+	let!(:wrong_exercise_hypothesis){FactoryGirl.create(:exercise_hypothesis, hypothesis:hypothesis, exercise:exercise)}
+
 
 	let(:valid_attributes) {
-		{subtask_id: subtask.id, title: "Viimeinen kysymys"}
+		{subtask_id: subtask.id, title: "Viimeinen kysymys", exercise_hypothesis_id: exercise_hypothesis.id}
 	}
 
 	let(:invalid_attributes) {
@@ -19,7 +21,7 @@ RSpec.describe ConclusionsController, :type => :controller do
 	}
 
 	let(:valid_session) { {
-			user_id: user.id, task_id: task.id, exercise_id: exercise.id}
+			user_id: user.id, task_id: task.id, exercise_id: exercise.id, exhyp_id: exercise_hypothesis.id}
 	}
 
 	describe "GET new" do
@@ -37,6 +39,18 @@ RSpec.describe ConclusionsController, :type => :controller do
 				}.to change(Conclusion, :count).by(1)
 			end
 		end
+
+		describe "with invalid params" do
+	      it "assigns a newly created but unsaved conclusion as @conclusion" do
+	        post :create, {:conclusion => invalid_attributes}, valid_session
+	        expect(assigns(:conclusion)).to be_a_new(Conclusion)
+	      end
+
+	      it "redirects to the 'new' template" do
+	        post :create, {:conclusion => invalid_attributes}, valid_session
+	        expect(response).to redirect_to(new_conclusion_path(:layout => true))
+	      end
+	    end
 	end
 
 	describe "PUT update" do
@@ -68,39 +82,45 @@ RSpec.describe ConclusionsController, :type => :controller do
 	end
 
 	    describe "check_answers" do
-    #	let(:conclusion){FactoryGirl.create(:conclusion, subtask:subtask)}
 
-   #  	it "completes subtask" do
-			# expect {
-   #  			post :check_answers, {:id => conclusion.to_param}, valid_session
-   #  		}.to change(CompletedSubtask, :count).by(1)
-   #  	end
+	    	it "completes subtask" do
+				expect {
+					conclusion = Conclusion.create! valid_attributes
+	     			post :check_answers, {:id => conclusion.to_param, :exhyp_id => conclusion.exercise_hypothesis.id}, valid_session
+	     		}.to change(CompletedSubtask, :count).by(1)
+	     	end
 
-   #  	let(:question){FactoryGirl.create(:question, interview:interview)}
-    	
+	     	 it "does not complete subtask if wrong hypothesis is chosen" do
+				expect {
+					conclusion = Conclusion.create! valid_attributes
+	     			post :check_answers, {:id => conclusion.to_param, :exhyp_id => wrong_exercise_hypothesis.id }, valid_session
+	     		}.not_to change(CompletedSubtask, :count)
+	     	end
 
-   #  	 it "does not complete subtask if required questions not asked" do
-			# expect {
-   #  			post :check_answers, {:id => interview.to_param}, valid_session
-   #  		}.not_to change(CompletedSubtask, :count)
-   #  	end
-
-    end
+   	 end
 
     	describe "DELETE create" do
-		    it "destroys the requested conclusion" do
-		      conclusion = Conclusion.create! valid_attributes
-		      expect {
-		        delete :destroy, {:id => conclusion.to_param}, valid_session
-		      }.to change(Conclusion, :count).by(-1)
-		    end
 
-		    it "redirects to the task list" do
-		      conclusion = Conclusion.create! valid_attributes
-		      delete :destroy, {:id => conclusion.to_param}, valid_session
-		      expect(response).to redirect_to(tasks_url(:layout => true))
-		    end
+		    	it "destroys the requested conclusion" do
+		    	conclusion = Conclusion.create! valid_attributes
+		      	expect {
+		        	delete :destroy, {:id => conclusion.to_param}, valid_session
+		      	}.to change(Conclusion, :count).by(-1)
+		    	end
 
-	end
+		    	it "destroys the corresponding Task" do
+		    	conclusion = Conclusion.create! valid_attributes
+		      	expect {
+		        	delete :destroy, {:id => conclusion.to_param}, valid_session
+		      	}.to change(Task, :count).by(-1)
+		    	end
+
+			    it "redirects to the task list" do
+			      conclusion = Conclusion.create! valid_attributes
+			      delete :destroy, {:id => conclusion.to_param}, valid_session
+			      expect(response).to redirect_to(tasks_url(:layout => true))
+			    end
+
+		end
 
 end
