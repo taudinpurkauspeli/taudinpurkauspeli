e0d925729b42905750ce55057b97d4fc05c777da
41407 <jimi.welling@cs.helsinki.fi>
2015-04-24 12:11:34 +0300

    Conclusion test cases

--- spec/models/conclusion_spec.rb
+++ spec/models/conclusion_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Conclusion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has overridden to_s" do
+    conclusion = Conclusion.new
+    expect(conclusion.to_s).to eq("Päätöstoimenpide")
+  end
+
+  describe "user_answered_correctly?" do
+      
+    describe "when user answers wrong" do
+
+      it "returns false" do
+        expect(0).to eq (0)
+      end
+
+      it "doesn't add completed_subtask to user" do
+        expect(0).to eq (0)
+      end
+    end
+
+    describe "when user answers correct" do
+
+      it "returns true" do
+        expect(0).to eq (0)
+      end
+
+      it "adds completed_subtask to user" do
+        expect(0).to eq (0)
+      end
+    end
+  end
 end
