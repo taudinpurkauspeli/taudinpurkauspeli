5b8c9db7243e113aae8ba2409f7ecd4f948c6744
Eveliina <pevetiainen.ep@hotmail.com>
2015-04-26 21:40:26 +0300

    cucumber tests for user

--- features/step_definitions/user_index_steps.rb
+++ features/step_definitions/user_index_steps.rb
@@ -12,11 +12,30 @@ Given(/^some tasks have been added to case$/) do
 	create_task_texts
 end
 
+Given(/^students have created accounts$/) do
+	create_students
+end
+
 Given(/^a student has one task completed$/) do
 	complete_task(Task.find(3), User.find(1))
 end
 
+Given(/^students have completed tasks$/) do
+	complete_task(Task.find(3), User.find(2))
+	complete_task(Task.find(5), User.find(3))
+end
+
 When(/^"(.*?)" option is selected from case list$/) do |arg1|
 	select(arg1, from:'exercise')
+end
+
+When(/^I select "(.*?)" to be shown$/) do |arg1|
+	select(arg1, from:'list_type')
+end
+
+Then(/^the page should show all students$/) do
+	expect(page).to have_content(User.find(2).realname)
+	expect(page).to have_content(User.find(3).realname)
+	expect(page).to have_content(User.find(4).realname)
 end
 
