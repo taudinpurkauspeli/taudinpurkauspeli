f4fd60fe40dd4ba1a67d6dfa9e9e946166b69e23
41407 <jimi.welling@cs.helsinki.fi>
2015-04-27 14:02:19 +0300

    Remove unused cucumber step definition files

--- features/step_definitions/anamnesis_steps.rb
+++
@@ -1,8 +0,0 @@
-begin
-  require 'rspec/expectations';
-rescue LoadError;
-  require 'spec/expectations';
-end
-require 'cucumber/formatter/unicode'
-$:.unshift(File.dirname(__FILE__) + '/../../lib')
-
