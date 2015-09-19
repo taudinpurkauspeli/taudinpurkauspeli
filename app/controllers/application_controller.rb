a1aac82e1c889b7593b5e20b4b9c59877c2cabf9
Eveliina <pevetiainen.ep@hotmail.com>
2015-09-19 23:10:24 +0300

    Logging only production when student signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
 
-  around_action :log_request, if: :current_user_is_student
+  around_action :log_request, if: :current_user_is_student_in_production
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
     return nil if session[:task_id].nil?
     Task.find(session[:task_id])
   end
-  
+
   def ensure_user_is_logged_in
     redirect_to exercises_path, alert: "Toiminto vaatii sisäänkirjautumisen" if current_user.nil?
   end
@@ -54,32 +54,44 @@ class ApplicationController < ActionController::Base
   private
 
   def log_request
-    # if !current_user.try(:admin)
-    #   logged_item = {}
-    #   logged_item[:user_id] = current_user.id
-    #   logged_item[:controller] = params[:controller]
-    #   logged_item[:action] = params[:action]
-    #   logged_item[:params] = params
-    #   logged_item[:exercise_id] = session[:exercise_id]
-    #   logged_item[:task_id] = session[:task_id]
-    #   logged_item[:exhyp_ids] = session[:exhyp_ids]
-    #   logged_item[:datetime] = DateTime.current
-    #   logged_item[:path] = request.path
-    #   logged_item[:ip] = request.ip
-    #   logged_item[:method] = request.method
-    # end
-     yield
-    # logged_item[:response] = response.location
 
-    #byebug
+    new_log_entry = LogEntry.new
+
+    new_log_entry.user_id = current_user.id
+    new_log_entry.controller = params[:controller]
+    new_log_entry.action = params[:action]
+
+    compressed_params = YAML::dump(params)
+    new_log_entry.params = compressed_params
+
+    new_log_entry.exercise_id = session[:exercise_id]
+    new_log_entry.task_id = session[:task_id]
+
+    compressed_exhyp_ids = YAML::dump(session[:exhyp_ids])
+    new_log_entry.exhyp_ids = compressed_exhyp_ids
+
+    new_log_entry.datetime = DateTime.current
+    new_log_entry.request_path = request.url
+    new_log_entry.ip = request.ip
+    new_log_entry.method = request.method
+
+    yield
+
+    new_log_entry.response_path = response.location
+
+    new_log_entry.save
+
   end
 
-  def current_user_is_student
-    user = current_user
-    if user.nil? || user.admin
-      return false
+  def current_user_is_student_in_production
+    if Rails.env.production?
+      user = current_user
+      if user.nil? || user.admin
+        return false
+      end
+      return true
     end
-    return true
+    return false
   end
 
 end
