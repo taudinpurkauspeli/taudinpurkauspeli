7e8f1a6a9b951244045211c0cb3f6638db708961
Eveliina <pevetiainen.ep@hotmail.com>
2015-09-24 23:23:22 +0300

    Fixed ask interview question problem

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -16,14 +16,14 @@ class InterviewsController < ApplicationController
 		@new_asked_question = AskedQuestion.new
 		@new_question_group = QuestionGroup.new
 		@question_groups = QuestionGroup.all
-    @requireds = Question.requireds
+		@requireds = Question.requireds
 
-    set_view_layout
-  end
+		set_view_layout
+	end
 
 
-  def create
-    @task = Task.find(session[:task_id])
+	def create
+		@task = Task.find(session[:task_id])
 
 		# This can be done for each different type of subtask in their respective controllers
 		subtask = @task.subtasks.build
@@ -55,38 +55,44 @@ class InterviewsController < ApplicationController
 	end
 
 	def ask_question
-		current_user.ask_question(Question.find(question_params[:question_id]))
+		question = Question.find(question_params[:question_id])
+		current_user.ask_question(question)
+		#byebug
 		respond_to do |format|
-			format.html { redirect_to task_path(@interview.subtask.task, :layout => get_layout, :last_clicked_question_id => question_params[:question_id]) }
+			if question_params[:exercise_show_or_task_show] == ("task_show" + question.interview.id.to_s)
+				format.html { redirect_to task_path(@interview.subtask.task, :layout => get_layout, :last_clicked_question_id => question_params[:question_id]) }
+			else
+				format.html { redirect_to exercise_path(current_exercise, :layout => get_layout, :last_clicked_question_id => question_params[:question_id]) }
+			end
 		end
 	end
 
-  # TODO fix user_has_completed redirect logic
+	# TODO fix user_has_completed redirect logic
 	def check_answers
 		respond_to do |format|
 			if @interview.all_questions_asked_by?(current_user)
 				current_user.complete_subtask(@interview.subtask)
-        if(current_user.has_completed?(current_exercise))
-          format.html { redirect_to task_path(@interview.subtask.task, :layout => get_layout, notice: "Onneksi olkoon suoritit casen!") }
-        else
-          format.html { redirect_to task_path(@interview.subtask.task, :layout => get_layout) }
-        end
-      else
-        format.html { redirect_to task_path(@interview.subtask.task, :layout => get_layout), alert: 'Et ole vielä valinnut kaikkia tarpeellisia vaihtoehtoja!' }
-      end
-    end
-  end
+				if(current_user.has_completed?(current_exercise))
+					format.html { redirect_to task_path(@interview.subtask.task, :layout => get_layout, notice: "Onneksi olkoon suoritit casen!") }
+				else
+					format.html { redirect_to task_path(@interview.subtask.task, :layout => get_layout) }
+				end
+			else
+				format.html { redirect_to task_path(@interview.subtask.task, :layout => get_layout), alert: 'Et ole vielä valinnut kaikkia tarpeellisia vaihtoehtoja!' }
+			end
+		end
+	end
 
-  private
-  def set_interview
-    @interview = Interview.find(params[:id])
-  end
+	private
+	def set_interview
+		@interview = Interview.find(params[:id])
+	end
 
-  def question_params
-    params.permit(:question_id)
-  end
+	def question_params
+		params.permit(:question_id, :exercise_show_or_task_show)
+	end
 
-  def interview_params
-    params.require(:interview).permit(:title)
-  end
+	def interview_params
+		params.require(:interview).permit(:title)
+	end
 end
