3c1637b60aeaf6bbd4303772b3258d520a1d8ab4
Eveliina <pevetiainen.ep@hotmail.com>
2015-11-01 00:39:59 +0200

    Click method order change

--- spec/support/wait_for_ajax.rb
+++ spec/support/wait_for_ajax.rb
@@ -51,14 +51,6 @@ module WaitForAjax
     wait_for_ajax
 
     begin
-      click_link_or_button(element)
-      wait_for_ajax
-      return
-    rescue
-      # puts "Clickable thing '" + element_text + "' not found"
-    end
-
-    begin
       find_button(element).trigger('click')
       wait_for_ajax
       return
@@ -72,6 +64,14 @@ module WaitForAjax
       return
     rescue
      # puts "Link '" + element + "' not found in DOM"
+    end
+    
+    begin
+      click_link_or_button(element)
+      wait_for_ajax
+      return
+    rescue
+      # puts "Clickable thing '" + element_text + "' not found"
     end
 
     raise "Element containing text '" + element + "' not found in DOM"
