c4c94e92649214fbca6bf4eac1c3bdab704e3881
Eveliina <pevetiainen.ep@hotmail.com>
2015-11-02 21:03:48 +0200

    Added angular_test according to the book

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,8 @@ Rails.application.routes.draw do
 
   resources :conclusions
 
+  get "angular_test", to: "angular_test#index"
+
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#destroy'
 
