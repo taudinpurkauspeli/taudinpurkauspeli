88dddcc68f8ca04d96058343ceb7a748ace27441
Eveliina <pevetiainen.ep@hotmail.com>
2015-11-02 22:04:46 +0200

    Display list in search results

--- app/assets/javascripts/taudinpurkauspeli_app.js
+++ app/assets/javascripts/taudinpurkauspeli_app.js
@@ -1,8 +1,20 @@
 var app = angular.module('taudinpurkauspeli',[ ]);
 
 var HypothesisGroupSearchController = function($scope) {
+    $scope.hypothesisGroups = [];
     $scope.search = function(searchTerm) {
-        $scope.searchedHypothesisGroup = searchTerm;
+        $scope.hypothesisGroups = [
+            {
+                "name":"Bakteeritaudit"
+
+            },
+            {
+                "name":"Virustaudit"
+            },
+            {
+                "name":"Nuhataudit"
+            }
+        ]
     }
 };
 
