cef5bce70d6e46c5526a0999ae83cd0afbb999ea
Eveliina <pevetiainen.ep@hotmail.com>
2015-11-02 21:54:35 +0200

    Adding hypothesisgroupsearch view

--- app/assets/javascripts/taudinpurkauspeli_app.js
+++ app/assets/javascripts/taudinpurkauspeli_app.js
@@ -1,1 +1,11 @@
-angular.module('taudinpurkauspeli',[ ]);
+var app = angular.module('taudinpurkauspeli',[ ]);
+
+var HypothesisGroupSearchController = function($scope) {
+    $scope.search = function(searchTerm) {
+        $scope.searchedHypothesisGroup = searchTerm;
+    }
+};
+
+app.controller("HypothesisGroupSearchController",
+    [ "$scope", HypothesisGroupSearchController ]
+);
