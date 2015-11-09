c23979d3b31996f4cafd819e7d5864f4f89eb43c
Eveliina <pevetiainen.ep@hotmail.com>
2015-11-09 18:37:37 +0200

    Exercises index edited

--- app/assets/javascripts/controllers/exercises/exercises_controller.js
+++ app/assets/javascripts/controllers/exercises/exercises_controller.js
@@ -1,8 +1,11 @@
 var app = angular.module('diagnoseDiseases');
 
 app.controller("ExercisesController", [
-    "$scope","$http","$routeParams", "$resource",
-    function($scope , $http , $routeParams, $resource) {
-        $scope.exercise = "Taudinpurkauspelin caset";
+    "$scope","$http","$routeParams", "$resource", "$location",
+    function($scope , $http , $routeParams, $resource, $location) {
+        $scope.exercisesList = [];
+
+        var Exercises = $resource('/exercises.json');
+        $scope.exercisesList = Exercises.query();
     }
 ]);
