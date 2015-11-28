e579c5e70c317188adb911adb86bbf76615a6313
Eveliina <pevetiainen.ep@hotmail.com>
2015-11-28 20:27:05 +0200

    Link visibility with ng-show

--- app/assets/javascripts/controllers/application/application_controller.js
+++ app/assets/javascripts/controllers/application/application_controller.js
@@ -3,6 +3,17 @@ var app = angular.module('diagnoseDiseases');
 app.controller("ApplicationController", [
     "$scope","$http","$routeParams", "$resource", "$location", "AuthenticationService",
     function($scope , $http , $routeParams, $resource, $location, AuthenticationService) {
+        $scope.navigationLinksList = [
+            {path: "#/",
+                title: "Taudinpurkauspeli",
+                loggedIn: true,
+                visibility: "currentUser"},
+            {path: "#/hypothesis_groups",
+                title: "Diffiryhmät",
+                loggedIn: true,
+                visibility: "currentUserAdmin"}
+        ];
+
         $scope.currentUser = AuthenticationService.isLoggedIn();
         $scope.currentUserAdmin = AuthenticationService.isAdmin();
 
