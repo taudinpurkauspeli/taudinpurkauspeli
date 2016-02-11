250306b3efb46b2dc1e744d8fa461625ee6b88a1
Eveliina <pevetiainen.ep@hotmail.com>
2016-02-11 19:38:38 +0200

    Updated angular dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require angular
-//= require angular-route
 //= require angular-resource
 //= require angular-messages
 //= require angular-rails-templates
