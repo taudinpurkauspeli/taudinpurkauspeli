0e0a46e99b11a669a23dca213a108fe99081619e
Eveliina <pevetiainen.ep@hotmail.com>
2016-02-14 14:31:13 +0200

    Authentication controller reviewed

--- app/assets/javascripts/controllers/application/authentication_controller.js
+++ app/assets/javascripts/controllers/application/authentication_controller.js
@@ -4,32 +4,31 @@ app.controller("AuthenticationController", [
     "$scope", "AuthenticationService", "$window",
     function($scope, AuthenticationService, $window) {
 
-        $scope.credentials = {
-            username: '',
-            password: ''
+        $scope.credentials = {};
+
+        $scope.resetCredentials = function() {
+            $scope.credentials = {
+                username: '',
+                password: ''
+            };
         };
 
+        $scope.resetCredentials();
+
         $scope.login = function(credentials) {
-            AuthenticationService.login(credentials).success(function (user) {
+            AuthenticationService.login(credentials).success(function() {
                 $scope.setCurrentUser(AuthenticationService.isLoggedIn(), AuthenticationService.isAdmin());
-                $scope.credentials = {
-                    username: '',
-                    password: ''
-                };
-            }).error(function () {
-                $scope.credentials = {
-                    username: '',
-                    password: ''
-                };
+                $scope.resetCredentials();
+            }).error(function() {
+                $scope.resetCredentials();
             });
         };
 
-        $scope.logout = function (){
-            AuthenticationService.logout().success(function () {
+        $scope.logout = function() {
+            AuthenticationService.logout().success(function() {
                 $scope.setCurrentUser(AuthenticationService.isLoggedIn(), AuthenticationService.isAdmin());
-
                 $window.alert("Uloskirjautuminen onnistui");
-            }).error(function () {
+            }).error(function() {
                 $window.alert("Uloskirjautuminen epäonnistui");
             });
         }
--- app/assets/javascripts/services/authentication_service.js
+++ app/assets/javascripts/services/authentication_service.js
@@ -1,39 +1,37 @@
 var app = angular.module('diagnoseDiseases');
 
 app.factory('AuthenticationService', [
-    "$http","Session", "$location",
-    function ($http, Session, $location) {
+    "$http","Session", "$state",
+    function ($http, Session, $state) {
         var authService = {};
 
-        authService.login = function (credentials) {
+        authService.login = function(credentials) {
             return $http
                 .post('/sessions.json', credentials)
-                .success(function (data,status,headers,config) {
+                .success(function(data,status,headers,config) {
                     Session.create(data);
-                    //alert(Session.userId());
                     return data;
-                }).error(function(data,status,headers,config){
+                }).error(function(data,status,headers,config) {
                     alert("Kirjautuminen epäonnistui");
                 });
         };
 
-        authService.logout = function () {
+        authService.logout = function() {
             return $http
                 .delete('/signout.json')
-                .success(function (data,status,headers,config) {
+                .success(function(data,status,headers,config) {
                     Session.destroy();
-                    //alert(Session.userId());
-                    $location.path("/");
-                }).error(function(data,status,headers,config){
+                    $state.go('app_root');
+                }).error(function(data,status,headers,config) {
                     alert("Uloskirjautuminen epäonnistui");
                 });
         };
 
-        authService.isLoggedIn = function () {
+        authService.isLoggedIn = function() {
             return Session.userId();
         };
 
-        authService.isAdmin = function () {
+        authService.isAdmin = function() {
             return Session.userAdmin() === "true";
         };
 
