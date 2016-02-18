a9e656e77930b9f88b0ec19df81401edbeafdb62
Eveliina <pevetiainen.ep@hotmail.com>
2016-02-19 00:06:40 +0200

    Exercise show tabs and views in lists

--- app/assets/javascripts/controllers/exercises/exercises_show_controller.js
+++ app/assets/javascripts/controllers/exercises/exercises_show_controller.js
@@ -5,6 +5,33 @@ app.controller("ExercisesShowController", [
     function($scope, $stateParams, $resource, LocalStorageService) {
         $scope.exercise = {};
 
+        $scope.views = [
+            {viewName: "anamnesis_tab",
+                tabId: "1",
+                visibility: true},
+            {viewName: "task_list",
+                tabId: "2",
+                visibility: true},
+            {viewName: "exercise_hypothesis_list",
+                tabId: "3",
+                visibility: true}
+        ];
+
+        $scope.links = [
+            {tabName: "AnamnesisTab",
+                tabId: "1",
+                title: "Anamneesi",
+                visibility: true},
+            {tabName: "TaskTab",
+                tabId: "2",
+                title: "Toimenpidelista",
+                visibility: true},
+            {tabName: "HypothesisTab",
+                tabId: "3",
+                title: "Diffilista",
+                visibility: true}
+        ];
+
         var exerciseId = $stateParams.id;
         var ExerciseOne = $resource('/exercises_one/:exerciseId.json',
             { exerciseId: "@id"});
@@ -22,7 +49,7 @@ app.controller("ExercisesShowController", [
         };
 
         $scope.setCurrentTab();
-        
+
         $scope.changeCurrentTab = function(newTab, tabID) {
             $scope.current_tab = newTab;
             LocalStorageService.set("current_tab", newTab);
