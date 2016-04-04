5701f1e1c14f87dc04c598b0af1e1738e8d0fdc6
Eveliina <pevetiainen.ep@hotmail.com>
2016-04-04 22:48:24 +0300

    Add sorting for list

--- app/assets/javascripts/controllers/users/users_controller.js
+++ app/assets/javascripts/controllers/users/users_controller.js
@@ -4,6 +4,7 @@ app.controller("UsersController", [
     "$scope", "$resource",
     function($scope, $resource) {
         $scope.users = {};
+        $scope.orderByAttribute = ['-starting_year', 'first_name', 'last_name'];
 
         var Users = $resource('/users_json.json');
 
@@ -14,5 +15,9 @@ app.controller("UsersController", [
         };
 
         $scope.setUsers();
+
+        $scope.setOrderByAttribute = function(newAttribute){
+            $scope.orderByAttribute[0] = newAttribute;
+        }
     }
 ]);
