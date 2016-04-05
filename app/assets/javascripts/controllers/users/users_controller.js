0a4f2ccf552830056b76f467bbf1c59f80f825b0
Eveliina <pevetiainen.ep@hotmail.com>
2016-04-06 00:04:33 +0300

    Better looking sorting student list

--- app/assets/javascripts/controllers/users/users_controller.js
+++ app/assets/javascripts/controllers/users/users_controller.js
@@ -4,7 +4,8 @@ app.controller("UsersController", [
     "$scope", "$resource",
     function($scope, $resource) {
         $scope.users = {};
-        $scope.orderByAttribute = ['-starting_year', 'first_name', 'last_name'];
+        $scope.orderByAttribute = ['starting_year', '-first_name', '-last_name'];
+        $scope.reverse = true;
 
         var Users = $resource('/users_json.json');
 
@@ -17,6 +18,8 @@ app.controller("UsersController", [
         $scope.setUsers();
 
         $scope.setOrderByAttribute = function(newAttribute){
+            var attributeSameAsFirstOrderByAttribute = ($scope.orderByAttribute[0] === newAttribute);
+            $scope.reverse = attributeSameAsFirstOrderByAttribute ? !$scope.reverse : false;
             $scope.orderByAttribute[0] = newAttribute;
         }
     }
