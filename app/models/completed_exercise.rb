485f52252175a66314494a6df4e9f3b627b85dc6
Eveliina <pevetiainen.ep@hotmail.com>
2016-04-07 00:16:35 +0300

    Add validations to completed exercise, task and subtask not to complete same thing twice

--- app/models/completed_exercise.rb
+++ app/models/completed_exercise.rb
@@ -1,4 +1,18 @@
 class CompletedExercise < ActiveRecord::Base
   belongs_to :user
   belongs_to :exercise
+
+  validate :cannot_complete_same_exercise_many_times
+
+  def cannot_complete_same_exercise_many_times
+
+    if user_id.present? && exercise_id.present?
+      how_many_completed_exercises = CompletedExercise.where(user_id: user_id, exercise_id: exercise_id)
+      if how_many_completed_exercises.count > 0
+        errors.add(:base, 'User has already completed this exercise')
+      end
+    end
+  end
+
 end
+
