f9f1a21dacee9516ead08a464429e1ff05d1f9f4
Eveliina <pevetiainen.ep@hotmail.com>
2016-05-29 00:55:08 +0300

    Started complete task text part

--- app/assets/javascripts/controllers/tasks/tasks_completed_subtask_controller.js
+++ app/assets/javascripts/controllers/tasks/tasks_completed_subtask_controller.js
@@ -1,8 +1,24 @@
 var app = angular.module('diagnoseDiseases');
 
 app.controller("TasksCompletedSubtaskController", [
-    "$scope", "$stateParams", "LocalStorageService",
+    "$scope", "$stateParams",
     function($scope, $stateParams) {
+
+        $scope.setSubtask = function() {
+            if($scope.subtask.task_text){
+                return;
+            } else if($scope.subtask.multichoice){
+                //get options
+
+            } else if($scope.subtask.interview){
+                //get questions
+
+            } else if($scope.subtask.conclusion){
+                //get something
+            }
+        };
+
+
 
     }
 ]);
--- app/assets/javascripts/controllers/tasks/tasks_uncompleted_subtask_controller.js
+++ app/assets/javascripts/controllers/tasks/tasks_uncompleted_subtask_controller.js
@@ -1,8 +1,22 @@
 var app = angular.module('diagnoseDiseases');
 
 app.controller("TasksUncompletedSubtaskController", [
-    "$scope", "$stateParams", "LocalStorageService",
+    "$scope", "$stateParams",
     function($scope, $stateParams) {
+
+        $scope.setSubtask = function() {
+            if($scope.subtask.task_text){
+                return;
+            } else if($scope.subtask.multichoice){
+                //get options
+
+            } else if($scope.subtask.interview){
+                //get questions
+
+            } else if($scope.subtask.conclusion){
+                //get something
+            }
+        };
 
     }
 ]);
