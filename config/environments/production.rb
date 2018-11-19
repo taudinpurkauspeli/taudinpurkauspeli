cc442a8d3610460645b706103501be5e023bd7da
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-11-19 10:21:50 +0200

    Change production storage path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.active_storage.service = :local
+  config.active_storage.service = :production
 end
